5f93bbbda7eafbaca8574d3aed00a78cd077261a
netzon-jlaw <jlawrence.cordova@netzon.com.se>
2021-01-03 12:09:13 +0800

    chore(cleanup): add startup xml docs

--- HomeBrewed/Startup.cs
+++ HomeBrewed/Startup.cs
@@ -18,14 +18,24 @@ namespace HomeBrewed
     /// </summary>
     public class Startup
     {
+        /// <summary>
+        ///  Application starting point.
+        /// </summary>
+        /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// Startup configuration.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -47,7 +57,11 @@ namespace HomeBrewed
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
